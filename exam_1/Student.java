50f4251cdda6bd0be2dae54cd6830526928b771c
Erhn1 <111546581+Erhn1@users.noreply.github.com>
2022-09-06 10:16:53 +0800

    Create Student.java

---
+++ exam_1/Student.java
@@ -0,0 +1,20 @@
+public class Student {
+    
+    String surname;
+    String firstName;
+    String middleInitial;
+    String dateOfBirth;
+    int studentNumber;
+    String studentEmailAddress;
+    boolean iAmAwesome;
+    
+    public void sayMyStudentNumber() {
+        System.out.println(studentNumber);
+    }
+    public void sayMystudentEmailAddress() {
+        System.out.println(studentEmailAddress);
+    }
+    public void sayiAmAwesome() {
+        System.out.println(iAmAwesome);
+    }
+}    
